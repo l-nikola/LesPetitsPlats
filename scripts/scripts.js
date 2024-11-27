a5dd949bae4b1ca3daa608d6853763b16465e81e
l-nikola <legay.nikola.pro@gmail.com>
2024-11-27 22:17:58 +0100

    feat : add search algo

---
+++ scripts/scripts.js
@@ -0,0 +1,37 @@
+function filterRecipes(searchTerm) {
+  const filteredRecipes = recipes.filter((recipe) => {
+    // Vérifier si le texte saisi est dans :
+    // Le titre de la recette
+    const name = recipe.name.toLowerCase().includes(searchTerm.toLowerCase());
+
+    // 2. Les ingrédients (recherche dans le tableau d'ingrédients)
+    const ingredient = recipe.ingredients.some((ingredient) =>
+      ingredient.ingredient.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    // 3. La description
+    const description = recipe.description
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    return name || ingredient || description;
+  });
+
+  // Afficher les recettes
+  console.clear(); // Remove
+  if (filteredRecipes.length > 0) {
+    console.log(filteredRecipes);
+  } else {
+    console.log(
+      `Aucune recette ne contient '${searchTerm}', vous pouvez chercher 'tarte aux pommes', 'poisson', etc.`
+    );
+  }
+}
+
+// Ajouter un écouteur d'événement sur la barre de recherche
+document.getElementById("searchBar").addEventListener("input", (event) => {
+  const searchTerm = event.target.value.trim(); // Récupérer le texte saisi
+  if (searchTerm.length > 3) {
+    filterRecipes(searchTerm); // Filtre les recettes
+  }
+});
